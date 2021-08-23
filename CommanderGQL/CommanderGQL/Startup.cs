ac5d13abdc2d10dfb402337c0fcf035bd64aadec
Marcelo Rocha <celoficial@hotmail.com>
2021-08-23 03:16:18 -0300

    Resolving Concurrent quering issue

--- CommanderGQL/CommanderGQL/Startup.cs
+++ CommanderGQL/CommanderGQL/Startup.cs
@@ -23,7 +23,7 @@ namespace CommanderGQL
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(_configuration.GetConnectionString("CommandConStr")));
+            services.AddPooledDbContextFactory<AppDbContext>(opt => opt.UseSqlServer(_configuration.GetConnectionString("CommandConStr")));
             services.AddGraphQLServer()
                 .AddQueryType<Query>();
 
